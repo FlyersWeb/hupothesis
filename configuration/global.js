1954e8b5103d2620a83075eb89cc701b12a699b7
FlyersWeb <contact@flyers-web.org>
2014-08-10 21:51:07 +0200

    Edited configuration

--- configuration/global.js
+++ configuration/global.js
@@ -23,7 +23,7 @@ var transporter = null;
 var global = {
 	app : {
 		regex : /(\d+)?d?(\d+)h(\d+)?m?/,
-		url : "http://www.timeup.com"
+		url : "http://www.hupothesis.com"
 	},
 	db : {
 		uri: process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/timeup'
@@ -33,8 +33,8 @@ var global = {
 		transporter: transporter
 	},
 	captcha : {
-		private_key: process.env.OPENSHIFT_RECAPTCHA_PRIVATE_KEY || '6LeGQPUSAAAAAA9JjXnT6yqbEIFPRPj6jjdN-P3k',
-		public_key: process.env.OPENSHIFT_RECAPTCHA_PUBLIC_KEY || '6LeGQPUSAAAAALTh6FWNUf96BnAWAkD2gswYbPJx',
+		private_key: process.env.OPENSHIFT_RECAPTCHA_PRIVATE_KEY || '',
+		public_key: process.env.OPENSHIFT_RECAPTCHA_PUBLIC_KEY || '',
 	}
 };
 
